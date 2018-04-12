13572ec7e3889ed5a09ffa9499a69b9cb09b0213
llack <akak343@naver.com>
2018-04-12 21:00:10 +0900

    dd

---
+++ issue/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<title>이슈 관리자</title>
+</head>
+<body>
+<?
+echo "ㅇㅇ";
+?>
+</body>
+</html>
