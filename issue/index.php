9a0288604554f7334ab1a41f10d0819e8b01c8b3
llack <akak343@naver.com>
2018-04-19 23:01:01 +0900

    purple

--- issue/index.php
+++ issue/index.php
@@ -8,7 +8,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/common/header.php";
 <div class="ui container side">
 <? include $_SERVER["DOCUMENT_ROOT"]."/common/company_list.php"; ?>
 </div>
-<div class="ui container table">
+<div class="ui container table purple segment">
 이슈관리자인데...
 </div>
 </body>
