412c206c606e5b0f7b5acec825544612aeed7b68
llack <akak343@naver.com>
2018-04-19 22:59:23 +0900

    d

--- issue/common/menu.php
+++ issue/common/menu.php
@@ -139,7 +139,7 @@
     <div class="ui ribbon  purple basic label">
       이름
     </div>
-    <input type="text" name="user_name" value="<?=$row_info[user_name]?>">
+    <input type="text" name="user_name" value="<?=$row_info[user_name]?>" onfocus="this.setSelectionRange(this.value.length, this.value.length)">
   </div>
   <div class="inline field">
     <div class="ui ribbon purple basic label">
@@ -151,13 +151,13 @@
     <div class="ui ribbon purple basic label">
       	비밀번호
     </div>
-    <input type="password" name="user_pwd" value="<?=$row_info[user_pw]?>" onfocus="this.value=''">
+    <input type="password" name="user_pwd" value="<?=$row_info[user_pw]?>">
   </div>
   <div class="inline field">
     <div class="ui ribbon purple basic label">
       	비밀번호확인
     </div>
-    <input type="password" name="user_pwd2" value="" onfocus="this.value=''">
+    <input type="password" name="user_pwd2" value="">
   </div>
   <div class="inline field">
     <div class="ui ribbon purple basic label">
